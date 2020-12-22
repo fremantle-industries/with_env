23c60899a1d94a95c778b0bde2d0906ffde1ea89
Alex Kwiatkowski <alex+git@fremantle.io>
2020-12-21 23:17:33 -0800

    Release v0.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule WithEnv.MixProject do
   def project do
     [
       app: :with_env,
-      version: "0.0.1",
+      version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       package: package(),
