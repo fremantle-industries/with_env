bced1b1db1c9b63d5466e24243a39a022d2f9f61
Alex Kwiatkowski <alex+git@fremantle.io>
2020-12-21 22:53:11 -0800

    Add put & delete

--- test/with_env_test.exs
+++ test/with_env_test.exs
@@ -1,8 +1,26 @@
 defmodule WithEnvTest do
   use ExUnit.Case
+  use WithEnv
   doctest WithEnv
 
-  test "greets the world" do
-    assert WithEnv.hello() == :world
+  test "can put values into the env" do
+    assert Application.get_env(:hello, :saying) == nil
+
+    with_env put: [hello: [saying: "galaxy"]] do
+      assert Application.get_env(:hello, :saying) == "galaxy"
+    end
+
+    assert Application.get_env(:hello, :saying) == nil
+  end
+
+  test "can delete values from the env" do
+    Application.put_env(:hello, :saying, "world")
+
+    with_env delete: [hello: [:saying]] do
+      assert Application.get_env(:hello, :saying) == nil
+    end
+
+    assert Application.get_env(:hello, :saying) == "world"
+    assert Application.delete_env(:hello, :saying) == :ok
   end
 end
